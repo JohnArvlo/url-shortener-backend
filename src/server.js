f6bbd3ac17b50afba3238a5524c2fec383f202f5
John <u202117377@upc.edu.pe>
2025-08-30 16:11:02 -0500

    fix: cors policy

--- src/server.js
+++ src/server.js
@@ -18,11 +18,18 @@ app.use(json());
 
 //cors
 // app.use(cors({origin: process.env.FRONTEND_URL})); //frontend url
+// app.use(cors({
+//   origin: [process.env.FRONTEND_URL, process.env.BASE_URL], 
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }));
+
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, process.env.BASE_URL], 
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 
 // ruta swagger UI
