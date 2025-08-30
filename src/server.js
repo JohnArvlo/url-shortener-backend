6c238496ff6df1a322ba2863302725530006df06
John <u202117377@upc.edu.pe>
2025-08-30 13:27:04 -0500

    feat: added start to package.json

--- src/server.js
+++ src/server.js
@@ -33,6 +33,5 @@ app.use("/", urlRoutes);
 
 
 //port
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 3000; // 👈 obligatorio en Azure
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
